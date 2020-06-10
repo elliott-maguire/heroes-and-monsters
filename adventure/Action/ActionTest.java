2a2c077fa984b349bb03fbad0137f725b9e2c28d
CerealThread <teleporter2145@gmail.com>
2020-06-10 12:35:54 -0700

    Fixed body of ActionTest.java

--- adventure/Action/ActionTest.java
+++ adventure/Action/ActionTest.java
@@ -1,4 +1,38 @@
 package adventure.Action;
 
-public class ActionTest {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    Action testAction = new Charge();
+
+    @Before
+    void setUpStream() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Test
+    void printName() {
+        testAction.printName();
+        assertEquals("Charge\n", outContent.toString());
+    }
+
+    @Test
+    void printDescription() {
+        testAction.printDescription();
+        assertEquals("A heavy, unstoppable attack with a run-up.\n", outContent.toString());
+    }
+
+    @After
+    void restoreStream() {
+        System.setOut(originalOut);
+    }
 }
