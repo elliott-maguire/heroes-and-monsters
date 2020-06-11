8eed9be85e0b93e7857b577ac100658f05be1636
eba39 <ealmaguer@eagles.ewu.edu>
2020-06-10 23:00:55 -0700

    monster factory changes

--- adventure/Character/Monster/MonsterFactory.java
+++ adventure/Character/Monster/MonsterFactory.java
@@ -3,6 +3,8 @@ package adventure.Character.Monster;
 import adventure.Action.ActionIndex;
 import adventure.Meta.MetaIndex;
 
+import java.util.Random;
+
 public class MonsterFactory {
     public static Monster makeOgre() {
         return new Ogre(MetaIndex.getOgreMeta(), ActionIndex.getStrikeAction(), ActionIndex.getQuickStrikeAction());
@@ -23,5 +25,24 @@ public class MonsterFactory {
 
     public static Monster makeZombie() {
         return new Zombie(MetaIndex.getZombieMeta(), ActionIndex.getStrikeAction(), ActionIndex.getQuickStrikeAction());
+    }
+
+    public static Monster makeRandom(){
+        Random rand = new Random();
+        int i = rand.nextInt(5);
+
+        switch(i){
+            case 0:
+                return makeOgre();
+            case 1:
+                return makeSkeleton();
+            case 2:
+                return makeSpider();
+            case 3:
+                return makeWitch();
+
+        }
+
+        return makeZombie();
     }
 }
